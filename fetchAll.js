00d99dc4fe0e0064534b7a85de1a04966b5d5e1f
blackChef <hongpidai@gmail.com>
2016-02-16 12:57:45 +0800

    remove aws

--- fetchAll.js
+++ fetchAll.js
@@ -1,28 +1,7 @@
 var Rx = require('rx');
 var _ = require('lodash');
 var moment = require('moment');
-var AWS = require('aws-sdk');
-
-AWS.config.update({
-  accessKeyId: 'AKIAIOTIHNQIMIC6CPVQ',
-  secretAccessKey: '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'
-});
-
-if (process.env.ISLOCAL == 1) {
-  console.log('use proxy');
-  var proxy = require('proxy-agent');
-  AWS.config.update({
-    httpOptions: {
-      agent: proxy('http://localhost:1080/')
-    }
-  });
-}
-
-var s3 = new AWS.S3();
-var s3params = {
-  'Bucket': 'blackchefjobs',
-  'Key': 'allResult.json'
-};
+var fs = require('fs-extra');
 
 function getCoreCompanyName(name) {
   return name
@@ -56,37 +35,18 @@ module.exports = function(src, callback) {
           });
         })
         .value();
-
-      s3.getObject(s3params, function(err, res) {
-        if (err) {
-          callback(err);
-          return;
-        }
-
-        var allResult = JSON.parse(res.Body.toString());
-        var newResult = latestResult.filter(function(item) {
-          return !allResult.find(function(allResultItem) {
-            return allResultItem.companyName == item.companyName;
-          });
+      var allResult = fs.readJSONSync('./allResult.json');
+      var newResult = latestResult.filter(function(item) {
+        return !allResult.find(function(allResultItem) {
+          return allResultItem.companyName == item.companyName;
         });
-
-        if (newResult.length > 0) {
-          allResult = allResult.concat(newResult);
-
-          s3.putObject(
-            Object.assign({}, s3params, {
-              'Body': JSON.stringify(allResult)
-            }),
-            function(err, res) {
-              if (err) {
-                console.log(`aws update json failed: ${err}`);
-              }
-            });
-        }
-
-        // dont wait for s3
-        callback(null, allResult, newResult);
       });
+
+      if (newResult.length > 0) {
+        allResult = allResult.concat(newResult);
+        fs.outputJSONSync('./allResult.json', allResult);
+        callback(null, allResult, newResult);
+      }
     },
 
     function(err) {
